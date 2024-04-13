d911e3f81525a5bfea4866f78cf3990e1c7b105b
naim-kim <143364203+naim-kim@users.noreply.github.com>
2024-04-13 23:56:41 +0900

    style: format

--- hw5/stack.cpp
+++ hw5/stack.cpp
@@ -1,56 +1,59 @@
 #include <iostream>
 #include <stack>
 #include <string>
+
 using namespace std;
 
-class MusicPlaylist {
+class Playlist {
 private:
     stack<string> playlist;
 
 public:
     void AddSong(const string& song) {
         playlist.push(song);
+        cout << "\"" << song << "\" successfully added to the playlist!" << endl;
     }
 
     void PlayCurrentSong() {
         if (!playlist.empty()) {
             cout << "Now playing: " << playlist.top() << endl;
+            cout << " ~ playing ~" << endl;
             playlist.pop();
         } else {
-            cout << "Playlist is empty" << endl;
+            cout << "No more songs to play :(" << endl;
         }
     }
 
-    // Print current playlist
     void PrintPlaylist() const {
         if (!playlist.empty()) {
-            cout << "Current Playlist:" << endl;
+            cout << "[ Current Playlist: ]" << endl;
             stack<string> temp = playlist;
             while (!temp.empty()) {
                 cout << temp.top() << endl;
                 temp.pop();
             }
         } else {
-            cout << "Playlist is empty" << endl;
+            cout << "No more songs to play :(" << endl;
         }
     }
 };
 
+
 int main() {
-    MusicPlaylist playlist;
+    Playlist playlist;
     
-    // Add songs to the playlist
     playlist.AddSong("Snooze - SZA");
     playlist.AddSong("Chi-Ka - Tabber");
     playlist.AddSong("Mala Fama - Danna Paola");
 
-    // Play the songs
-    cout << "--- Playing Playlist ---" << endl;
+    cout << "--- Naim's Playlist ---\n" << endl;
     playlist.PrintPlaylist();
+
+    cout << endl << "[ Playing songs from the playlist: ]" << endl;
     playlist.PlayCurrentSong();
     playlist.PlayCurrentSong();
     playlist.PlayCurrentSong();
-    playlist.PlayCurrentSong(); // Try to play when playlist is empty
+    playlist.PlayCurrentSong(); //when empty
 
     return 0;
 }
