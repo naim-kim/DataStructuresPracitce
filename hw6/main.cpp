b94934dcb65cf5d6bc3e2083fccbb39dd707f8b2
naim-kim <143364203+naim-kim@users.noreply.github.com>
2024-04-20 02:30:10 +0900

    :star:	new: add new feature
    loop program until entering option 3. Exit

--- hw6/main.cpp
+++ hw6/main.cpp
@@ -3,36 +3,36 @@
 #include "calc.h"
 
 int main() {
+    int choice = 0;
 
-    int choice;
-    std::cout << "Select Calculator Type:" << std::endl;
-    std::cout << "1. Postfix Calculator" << std::endl;
-    std::cout << "2. Regular Calculator" << std::endl;
-    std::cout << "Enter your choice: ";
-    std::cin >> choice;
+    while (choice != 3) {
+        std::cout << "Select Calculator Type:" << std::endl;
+        std::cout << "1. Postfix Calculator" << std::endl;
+        std::cout << "2. Regular Calculator" << std::endl;
+        std::cout << "3. Exit" << std::endl;
+        std::cout << "Enter your choice: ";
+        std::cin >> choice;
 
-    std::cin.ignore(); // Ignore newline character in buffer
+        std::cin.ignore(); // Ignore newline character in buffer
 
-    if (choice == 1) {
-        std::string str;
+        if (choice == 1) {
+            std::string str;
 
-        std::cout << "Input postfix expression: ";
-        std::getline(std::cin, str);    // reading one line from keyboard
+            std::cout << "Input postfix expression: ";
+            std::getline(std::cin, str);    // reading one line from keyboard
 
-        Calculator calc(str);
+            Calculator calc(str);
 
-        std::cout << "Result = " << calc.evaluate() << std::endl;
-    } 
-    /*else if (choice == 2) {
-        //string expression;
-        std::cout << "Not made yet.. :( sorry)";
-        //getline(cin, expression);
-
-        //Calculator regular_calc(expression);
-        //cout << "Result = " << regular_calc.evaluate() << endl;
-    } */
-    else {
-        std::cout << "Invalid choice." << std::endl;
+            std::cout << "Result = " << calc.evaluate() << std::endl;
+        } 
+        /*else if (choice == 2) {
+            std::cout << "Regular Calculator not implemented yet" << std::endl;
+        } */
+        else if (choice == 3) {
+            std::cout << "Exiting the program..." << std::endl;
+        } else {
+            std::cout << "Invalid choice. Please try again." << std::endl;
+        }
     }
 
     return 0;
