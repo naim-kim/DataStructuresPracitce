10c3ba9ec6c791ea3450164cda96baee9b6cdf42
naim-kim <143364203+naim-kim@users.noreply.github.com>
2024-04-25 23:39:51 +0900

    :star:  feat: make the user add the songs

--- Stack/stack.cpp
+++ Stack/stack.cpp
@@ -1,17 +1,40 @@
 #include <iostream>
 #include <stack>
 #include <string>
+#include <vector>
+#include <algorithm>
+#include <ctime> // for srand
 
 using namespace std;
 
 class Playlist {
 private:
     stack<string> playlist;
+    vector<string> shuffledSongs;
 
 public:
     void AddSong(const string& song) {
         playlist.push(song);
         cout << "\"" << song << "\" successfully added to the playlist!" << endl;
+    }
+
+    void ShufflePlaylist() {
+        // Copy songs from playlist to a vector
+        while (!playlist.empty()) {
+            shuffledSongs.push_back(playlist.top());
+            playlist.pop();
+        }
+        
+        // Shuffle the songs using random_shuffle
+        srand(time(0)); // Seed random number generator
+        random_shuffle(shuffledSongs.begin(), shuffledSongs.end());
+
+        // Push shuffled songs back to the playlist stack
+        for (const string& song : shuffledSongs) {
+            playlist.push(song);
+        }
+
+        cout << "Playlist shuffled successfully!" << endl;
     }
 
     void PlayCurrentSong() {
@@ -38,15 +61,26 @@ public:
     }
 };
 
-
 int main() {
     Playlist playlist;
-    
-    playlist.AddSong("Snooze - SZA");
-    playlist.AddSong("Chi-Ka - Tabber");
-    playlist.AddSong("Mala Fama - Danna Paola");
+    string songToAdd;
+    char choice;
+
+    do {
+        cout << "Enter the name of the song to add to the playlist: ";
+        getline(cin, songToAdd);
+        playlist.AddSong(songToAdd);
+
+        cout << "Do you want to add another song? (y/n): ";
+        cin >> choice;
+        cin.ignore(); // Clear input buffer
+    } while (choice == 'y' || choice == 'Y');
 
     cout << "--- Naim's Playlist ---\n" << endl;
+    playlist.PrintPlaylist();
+
+    cout << endl << "[ Shuffle Playlist: ]" << endl;
+    playlist.ShufflePlaylist();
     playlist.PrintPlaylist();
 
     cout << endl << "[ Playing songs from the playlist: ]" << endl;
