c3470f7c730ae763b5380127ab6ef11909b91de9
naim-kim <143364203+naim-kim@users.noreply.github.com>
2024-06-29 23:10:35 +0900

    Update main.cpp

--- Contact_Manager/main.cpp
+++ Contact_Manager/main.cpp
@@ -1,21 +1,23 @@
 #include <iostream>
 #include <string>
-#include "contact.h"
+#include "contact.h" // Assuming your header file is named contact.h
 
 using namespace std;
 
-#define MAX_NUM 1000 // can be given by standard input
+#define MAX_NUM 1000 // Can be given by standard input
 
 int main() {
     string file_name;
     Contact *contacts;
     contacts = new Contact(MAX_NUM);
 
-    cout << "file name of contact data:" << endl;
+    cout << "Enter the file name of contact data:" << endl;
     cin >> file_name;
 
     contacts->load(file_name);
     contacts->print();
+
+    delete contacts; // Free allocated memory before exit
 
     return 0;
 }
